6c32b568d5bf4123187b55aaf0f8cb6ae6187c0c
Hynek <25231320+Hajneken@users.noreply.github.com>
2025-08-05 13:30:08 +0200

    removing Raycast as it is not reliably synced

--- MAC/sync.sh
+++ MAC/sync.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 # This script synchronizes the contents of the .configfiles directory with the user's home directory.
-rsync -av --delete --exclude='.git' --exclude='.DS_Store' ./.configfiles/ ~/.configfiles/
+rsync -av --delete --exclude='.git' --exclude='.DS_Store' --exclude='.config/raycast' ./.configfiles/ ~/.configfiles/ 
